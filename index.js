33cf2a2bdfcd2342ea6730be81af5d98e8de5d8a
Lucas Sadilek <sadilek@gmail.com>
2019-04-19 15:07:40 +0200

    debug added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const wut = require('debug')('wut');
+const yep = require('debug')('yap');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('html'));
 app.get('/', (req, res) => res.sendFile("index.html"));
-app.listen(port, () => console.log(`listening on port ${port}`));
+wut('ok');
+yep('yes');
+app.listen(port, () => wut(`listening on port ${port}`));
