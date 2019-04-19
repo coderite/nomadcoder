b09e3c91322be9c34391e2d6c984436991689ce3
Lucas Sadilek <sadilek@gmail.com>
2019-04-19 10:52:44 +0200

    updated port settings index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-    app.get('/', (req, res) => res.send("hi there"));
+app.get('/', (req, res) => res.send("hi there"));
 app.listen(port, () => console.log(`listening on port ${port}`));
