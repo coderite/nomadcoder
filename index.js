bb6778697878bbaed3ec5e3c5d001d817247d388
Lucas Sadilek <sadilek@gmail.com>
2019-04-19 11:46:14 +0200

    added an HTML page

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send("hi there"));
+app.use(express.static('html'));
+app.get('/', (req, res) => res.sendFile("index.html"));
 app.listen(port, () => console.log(`listening on port ${port}`));
