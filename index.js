4a8a09a9c078df287a4aca516df96611df39a330
Lucas Sadilek <sadilek@gmail.com>
2019-04-20 19:59:19 +0200

    added BTCUSDT ticker

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const wut = require('debug')('wut');
-const yep = require('debug')('yap');
-
 const app = express();
+const wut = require('debug')('wut');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// some constants
 const port = process.env.PORT || 3000;
 
-app.use(express.static('html'));
-app.get('/', (req, res) => res.sendFile("index.html"));
-wut('ok');
-yep('yes');
+app.set('view engine', 'ejs');
+
+// app.use(express.static('html'));
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+const WebSocket = require('ws');
+
+const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@miniTicker');
+
+ws.on('message', function incoming(data) {
+    let ticker = JSON.parse(data);
+    console.log(ticker.type + ' ' + ticker.s + ' ' + ticker.c);
+});
+
 app.listen(port, () => wut(`listening on port ${port}`));
