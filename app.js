3f9e4e3791d4c579716293a6841be72e143c8e4c
Lucas Sadilek <zenbox@Lucass-MacBook-Pro.local>
2020-09-25 15:09:04 +0200

    added helmet security features

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const morgan = require('morgan');
 const favicon = require('serve-favicon');
 const path = require('path');
 const http = require('http');
+const helmet = require('helmet');
 
 // needs to be loaded before the DB models
 const passport = require('passport');
@@ -25,7 +26,6 @@ require('./api_server/config/passport');
 // constants
 const port = process.env.PORT;
 
-
 const routerApi = require('./api_server/routes/index');
 const routerApp = require('./app_server/routes/index');
 
@@ -35,7 +35,7 @@ app.set('views', 'app_server/views');
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public/css'));
 app.use(express.static('public/images'));
 app.use(express.static('public/js'));
@@ -45,32 +45,39 @@ app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(passport.initialize());
 
 /*
-*  ROUTING
-*/
+ *  ROUTING
+ */
 app.use('/api', routerApi);
 app.use('/', routerApp);
 
 // allow CORS
 app.use('/api', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With-Content-Type, Accept, Authorization');
-    next();
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With-Content-Type, Accept, Authorization'
+  );
+  next();
 });
+
+app.use(helmet());
 
 // catch all errors
 app.use((err, req, res, next) => {
-    if(err.name === 'UnauthorizedError') {
-        res.status(401).json({'message':'Forbidden'});
-    }
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ message: 'Forbidden' });
+  }
 });
 
 // heroku hack to prevent free tier from idling
-setInterval(function() {
-   http.get('http://www.nomadcoder.io');
+setInterval(function () {
+  http.get('http://www.nomadcoder.io');
 }, 300000);
 
-if(process.env.NODE_ENV === 'production') {
-    app.listen(port, () => debug(`listening on ${process.env.API_SERVER}`));
+if (process.env.NODE_ENV === 'production') {
+  app.listen(port, () => debug(`listening on ${process.env.API_SERVER}`));
 } else {
-    app.listen(port, () => debug(`listening on ${process.env.API_SERVER}:${port}`));
+  app.listen(port, () =>
+    debug(`listening on ${process.env.API_SERVER}:${port}`)
+  );
 }
