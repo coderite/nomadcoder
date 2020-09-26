d26742c604340b798e9adc8a1a4d352c5de17395
Lucas Sadilek <zenbox@Lucass-MacBook-Pro.local>
2020-09-26 12:26:53 +0200

    fixed csp issue

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ const helmet = require('helmet');
 //app.use(helmet());
 
 /** hemlet security features */
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ['*'],
-        fontSrc: ['*'],
-      },
-    },
-  })
-);
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 
 // needs to be loaded before the DB models
 
